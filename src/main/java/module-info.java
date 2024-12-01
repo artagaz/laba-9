d87fff90ba7648cab85c1afb1199db18f9178dad
artagaz <and.vaylin@gmail.com>
2024-12-01 22:45:47 +0700

    1

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,10 +2,6 @@ module org.example.laba9test {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires org.controlsfx.controls;
-    requires org.kordamp.bootstrapfx.core;
-    requires java.desktop;
-
     opens org.example.laba9test to javafx.fxml;
     exports org.example.laba9test;
 }
